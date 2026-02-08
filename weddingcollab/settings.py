039b388b6dd774ba1c40ad3d0bcb258885af2d26
emmepi97 <matteopoggesi23@gmail.com>
2026-02-08 20:23:27 +0100

    LAYOUT RESPONSIVE

--- weddingcollab/settings.py
+++ weddingcollab/settings.py
@@ -88,7 +88,8 @@ TIME_ZONE = "Europe/Rome"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
